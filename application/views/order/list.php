265f5e2ccdd074a7c98b2c464b52ad2b7771aa61
ArgeSwarge <argecata@mail.ru>
2016-01-22 09:27:35 +0200

    123 пуш просим

--- application/views/order/list.php
+++ application/views/order/list.php
@@ -334,20 +334,28 @@
                     <table class="table table-striped table-advance table-hover">
                         <tbody>
                         <tr>
+                            <th>Номер заказа(начинается с 100001)</th>
+                            <th>Дата</th>
                             <th>Клиент</th>
-                            <th>Дата доставки</th>
-                            <th>Статус заказа</th>
-                            <th>Тариф</th>
-                            <th>Оплата доставки</th>
+                            <th>Адрес отправителя</th>
+                            <th>Диспетчер</th>
+                            <th>Курьер</th>
+                            <th>Адрес получателя</th>
+                            <th>Диспетчер</th>
+                            <th>Курьер</th>
                             <th><i class="icon_cogs"></i>Действия</th>
                         </tr>
                         <? foreach($listOrders as $listOrder) { ?>
                             <tr>
-                                <td><?=$listOrder['client']?></td>
-                                <td><?=$listOrder['date']?></td>
-                                <td><?=$listOrder['order_state']?></td>
-                                <td><?=$listOrder['tariff']?></td>
-                                <td><?=$listOrder['payment']?></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
                                 <td>
                                     <div class="btn-group">
                                         <a class="btn btn-success editOrder" data-id="<?=$listOrder['id']?>" data-toggle="modal" data-target="#myModalEditOrder"  href="#"><i class="icon_cog"></i></a>
