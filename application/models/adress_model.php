8aa5632635f0b2a6442a7e4face99bd60016835f
Satuyan <ragnolur@gmail.com>
2016-05-22 21:35:19 +0300

    method getAdress add

--- application/models/adress_model.php
+++ application/models/adress_model.php
@@ -2,16 +2,43 @@
 
 class Adress_model extends CI_Model
 {
-
     public function __construct()
     {
         parent::__construct();
         $this->load->database();
     }
-   public function addAdress($data)
-   {
+    public function addAdress($data)
+    {
        if(!isset($data)){return false;}
        $this->db->insert("adress", $data);
        return $this->db->insert_id();
-   }
+    }
+    public function getAdress($parameters = array())
+    {
+        if(isset($parameters['ids'])){
+            $this->db->where_in('adress', $parameters['ids']);
+        }
+        if(isset($parameters['contragent_ids'])){
+            $this->db->where_in('id_contragent', $parameters['contragent_ids']);
+        }
+
+        $query = $this->db->get("adress");
+        $query = $query->result_array();
+
+        if (isset($parameters['list']) && $parameters['list'] == true) {
+            $query = $this->formatList($query);
+        }
+        return $query;
+    }
+
+    private function formatList($target = array())
+    {
+        foreach ($target as $value) {
+            $id = $value['id'];
+            $result[$id] = $value;
+        }
+        $target = $result;
+        return $target;
+    }
+
 }
